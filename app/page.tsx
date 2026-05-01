a8d8ec399385c7e13f240515a0701057ba9bfab3
bagoesrex <bagoesrex@gmail.com>
2026-05-02 03:56:22 +0700

    feat(layout): introduce max-width-wrapper and update background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import MaxWidthWrapper from "@/components/layout/max-width-wrapper";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="font-bold">The Regret Wall</h1>
-    </div>
+    <main className="min-h-screen">
+      <MaxWidthWrapper className="pt-20 pb-15">
+        <div className="flex flex-col items-center gap-4 text-center">
+          <h1 className="text-5xl font-bold">The Regret Wall</h1>
+          <p className="max-w-100">
+            Dinding anonim tempat semua orang meninggalkan penyesalan mereka. Tempelkan milikmu.
+          </p>
+        </div>
+      </MaxWidthWrapper>
+    </main>
   );
 }
