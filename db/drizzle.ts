49ce63344f22ba9b2218761a14dde4d9a93bec74
bagoesrex <bagoesrex@gmail.com>
2026-05-02 04:33:48 +0700

    feat(db): add regret seed script

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
@@ -7,3 +8,23 @@ const pool = new Pool({
 });
 
 export const db = drizzle(pool, { schema: schema });
+
+const regretTable = schema.regrets;
+
+async function main() {
+  const regret: typeof regretTable.$inferInsert = {
+    type: "text",
+    canvas: null,
+    message:
+      "I spent 3 years in a job I hated because I was too scared to leave. The day I finally quit was the best decision I ever made.",
+    color: "#c4947a",
+  };
+
+  await db.insert(regretTable).values(regret);
+  console.log("New regret created!");
+
+  const regrets = await db.select().from(regretTable);
+  console.log("Getting all regrets from the database: ", regrets);
+}
+
+main();
